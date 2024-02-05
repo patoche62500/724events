83c0e3e5414a740d0add1140e18a1a42f7cc25d9
patoche62500 <72212056+patoche62500@users.noreply.github.com>
2024-02-05 18:31:18 +0100

    changement de sens slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -9,7 +9,7 @@ const Slider = () => {
   const [index, setIndex] = useState(0);
 
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
 
   const nextCard = () => {
